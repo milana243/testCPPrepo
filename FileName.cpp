accb3790ea995d8f2d53a208a00ba947d75f23cc
p45 <p45@edu.itstep.lv>
2025-06-14 13:49:49 +0300

    Add a new function

---
+++ FileName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void i() {
+	cout << "i" << endl;
+}
+
+void main()
+{
+	cout << "Hello World!\n";
+	cout << "Hello gitHub!!!!" << endl;
+	cout << "danylo perykar" << endl;
+	i();
+}
